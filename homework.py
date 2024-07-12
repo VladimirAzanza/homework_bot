6bf62607f385beb2d9bf98265beb4acc1c3c433c
Vladimir Azanza <vladimirazanza@gmail.com>
2024-07-12 17:19:33 +0300

    Move all constants to constants.py file

--- homework.py
+++ homework.py
@@ -7,24 +7,20 @@ import requests
 from telebot import TeleBot, types
 from http import HTTPStatus
 
+from constants import (
+    PRACTICUM_TOKEN,
+    TELEGRAM_TOKEN,
+    TELEGRAM_CHAT_ID,
+    RETRY_PERIOD,
+    ENDPOINT,
+    HEADERS,
+    HOMEWORK_VERDICTS
+)
 from exceptions import (
     NoneValueException,
     StatusCodeException,
     UndefinedStatusException
 )
-
-
-PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-RETRY_PERIOD = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-HOMEWORK_VERDICTS = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
-}
 
 
 def check_tokens():
